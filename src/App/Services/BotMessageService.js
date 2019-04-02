d05780d8d2b7ac966d04388e733a3dbc8d0972e5
Azykin <elgr5212@gmail.com>
2019-04-02 15:31:37 +0300

    add log hook send message

--- src/App/Services/BotMessageService.js
+++ src/App/Services/BotMessageService.js
@@ -1,12 +1,15 @@
 'use strict';
 
 const BotMessage = require('../Models/BotMessage');
+const logger = require('../../Infrastructure/Logger');
 
 class BotMessageService{
 
     async sendMessage(message) {
 
         const result = await BotMessage.create(message);
+
+        logger.info(`Bot method sendMessage`, message);
 
         return result;
     }
